3f1fa0d010564f36ccddd0485c10a1871da891da
Michal Kaptur <kaptur.michal@gmail.com>
2018-11-12 15:44:34 +0100

    mod: further rework, extracted some common code

--- main.cpp
+++ main.cpp
@@ -28,6 +28,20 @@ bool update_score(std::size_t& benchmark, const auto left, const auto right)
     return false;
 }
 
+void lookup_impl(const auto begin, const auto end, auto left, auto right, std::size_t& benchmark)
+{
+    do {
+        if (not update_score(benchmark, left, right))
+        {
+            break;
+        }
+        std::advance(right, 1);
+        std::advance(left, -1);
+    }
+    while(right != end and left != begin);
+    update_score(benchmark, left, right);
+}
+
 std::size_t longest_even_palindrome_substring(const auto cursor, const std::string& input){
     std::size_t benchmark = 0;
     if (std::next(cursor) == input.end()){
@@ -42,15 +56,7 @@ std::size_t longest_even_palindrome_substring(const auto cursor, const std::stri
         return benchmark;
     }
 
-    do {
-        if (not update_score(benchmark, left, right)){
-            break;
-        }
-        std::advance(right, 1);
-        std::advance(left, -1);
-    }
-    while(right != input.end() and left != input.begin());
-    update_score(benchmark, left, right);
+    lookup_impl(input.begin(), input.end(), left,right, benchmark);
     return benchmark;
 }
 
@@ -67,17 +73,7 @@ std::size_t longest_odd_palindrome_substring(const auto cursor, const std::strin
         return 1;
     }
 
-
-    do {
-        if (not update_score(benchmark, left, right))
-        {
-            break;
-        }
-        std::advance(right, 1);
-        std::advance(left, -1);
-    }
-    while(right != input.end() and left != input.begin());
-    update_score(benchmark, left, right);
+    lookup_impl(input.begin(), input.end(), left,right, benchmark);
     return benchmark;
 }
 
