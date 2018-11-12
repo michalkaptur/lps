6c48e937a028fb6eb7d2d28a38ac06ccdc911dae
Michal Kaptur <kaptur.michal@gmail.com>
2018-11-12 17:28:23 +0100

    add: simple benchmark

--- benchmark.cpp
+++ benchmark.cpp
@@ -1,3 +1,16 @@
 #include <benchmark/benchmark.h>
+#include "longest_palindrome_substring.hpp"
+
+
+static void benchmark_lps(benchmark::State& state){
+    std::string s(state.range(), 'a'); //worst case
+    for (auto _ : state) {
+      benchmark::DoNotOptimize(longest_palindrome_substring(s));
+    }
+    state.SetComplexityN(state.range(0));
+}
+
+BENCHMARK(benchmark_lps)
+    ->RangeMultiplier(2)->Range(10, 500)->Complexity(benchmark::oN);
 
 BENCHMARK_MAIN();
