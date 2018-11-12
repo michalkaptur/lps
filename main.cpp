8c9d51f0beb6b032949f1d7d7f1cca0d02e19852
Michal Kaptur <kaptur.michal@gmail.com>
2018-11-12 11:27:07 +0100

    init

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+unsigned longest_palindrome_substring(const std::string& input){
+    return 0;
+}
+
+int main() {
+    std::string input("mightyracertwo");
+    std::cout << "Longest substring palindrome of " << input
+              << " has lenght:" << longest_palindrome_substring(input);
+    return 0;
+}
